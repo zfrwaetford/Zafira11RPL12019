3c9ef36d8114bf219c623e66f595d0fe1d9f9ef4
zfrwaetford <zafirawww@gmail.com>
2020-09-04 11:52:21 +0800

    JSON 5th Meeting

---
+++ app/src/main/java/com/example/zafira11rpl12019/ListData.java
@@ -0,0 +1,57 @@
+package com.example.zafira11rpl12019;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ListData extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private DataAdapter adapter;
+    private ArrayList<Model> DataArrayList; //kit add kan ke adapter
+    private ImageView tambah_data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_data);
+        recyclerView = (RecyclerView) findViewById(R.id.rvdata);
+        addData();
+}
+    void addData() {
+        //offline, isi data offline dulu
+        DataArrayList = new ArrayList<>();
+        Model data1 = new Model();
+        data1.setOriginal_title("Judul Film");
+        data1.setPoster_path("https://image.tmdb.org/t/p/w500/k68nPLbIST6NP96JmTxmZijEvCA.jpg");
+        data1.setAdult(false);
+        data1.setOverview("Deskripsi Film disini");
+        data1.setVote_count(100);
+        data1.setRelease_date("01-01-2020");
+        DataArrayList.add(data1);
+
+
+        adapter = new DataAdapter(DataArrayList, new DataAdapter.Callback() {
+            @Override
+            public void onClick(int position) {
+
+            }
+
+            @Override
+            public void test() {
+
+            }
+        });
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ListData.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        //get data online
+
+
+    }
+}
